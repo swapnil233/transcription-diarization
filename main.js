9cc0b4c11e51d205fd1e3e1fef53a52259c203b6
Hasan Iqbal <swapniliqbal@gmail.com>
2023-06-02 10:05:19 -0400

    ffmpeg stuff

--- main.js
+++ main.js
@@ -36,10 +36,12 @@ ipcMain.on("open-file-dialog", (event) => {
     })
     .then((result) => {
       if (!result.canceled) {
+        console.log(result.filePaths[0]);
         const videoPath = result.filePaths[0];
         const audioPath = videoPath.split(".").slice(0, -1).join(".") + ".wav";
 
         const ffmpeg = spawn("ffmpeg", [
+          "-y", // Add this line
           "-i",
           videoPath,
           "-vn",
@@ -51,14 +53,24 @@ ipcMain.on("open-file-dialog", (event) => {
           "2",
           audioPath,
         ]);
+
+        ffmpeg.stdout.on("data", (data) => {
+          console.log(`FFMPEG stdout: ${data.toString()}`);
+        });
+
+        ffmpeg.stderr.on("data", (data) => {
+          console.error(`FFMPEG stderr: ${data.toString()}`);
+        });
+
         ffmpeg.on("exit", (code) => {
           if (code === 0) {
-            const python = spawn("python3", ["transcribe.py", audioPath]);
+            const python = spawn("python", ["transcribe.py", audioPath]);
             python.stdout.on("data", (data) => {
-              console.log(`stdout: ${data}`);
+              console.log(`stdout: ${data.toString()}`);
             });
+
             python.stderr.on("data", (data) => {
-              console.error(`stderr: ${data}`);
+              console.error(`stderr: ${data.toString()}`);
             });
           }
         });
--- transcribe.py
+++ transcribe.py
@@ -1,16 +1,21 @@
 import sys
 import whisper
 
+
 def transcribe(audio_path):
+    print("Loading model...")
     model = whisper.load_model("base.en")
+    print("Model loaded, starting transcription...")
+
     result = model.transcribe(audio_path)
     text = result["text"]
-    
-    with open('transcript.txt', 'w') as f:
+
+    with open("transcript.txt", "w") as f:
         f.write(text)
 
-    print(f'Transcript saved to transcript.txt')
+    print(f"Transcript saved to transcript.txt")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     audio_path = sys.argv[1]
     transcribe(audio_path)
